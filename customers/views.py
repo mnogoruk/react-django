c152ad13c4456bdcea45ea7d69b6db1d1f80bd9b
mnogoruk <lionless072@gmail.com>
2021-01-28 01:09:36 +0300

    initial commit

---
+++ customers/views.py
@@ -0,0 +1,58 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Customer
+from .serializers import CustomerSerializer
+
+
+@api_view(['GET', 'POST'])
+def customers_list(request):
+    """
+ List  customers, or create a new customer.
+ """
+    if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, context={'request': request}, many=True)
+
+        return Response({'data': serializer.data, 'count': 1, 'numpages': 1,
+                         'nextlink': '/api/customers/?page=' + str(nextPage),
+                         'prevlink': '/api/customers/?page=' + str(previousPage)})
+
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def customers_detail(request, pk):
+    """
+ Retrieve, update or delete a customer by id/pk.
+ """
+    try:
+        customer = Customer.objects.get(pk=pk)
+        print(customer)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CustomerSerializer(customer, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
